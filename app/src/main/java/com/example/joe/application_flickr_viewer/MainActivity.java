313875a0d4b690a0caa56817bb4d2364c141a370
cor13136627 <cor13136627@student.citybathcoll.ac.uk>
2015-02-12 16:42:10 +0000

    List Working
    List View now working with 10 different cities in 10 different countries
    with pictures. buttons on main page work with going to different
    activities. Toast now have a meaningful value

--- app/src/main/java/com/example/joe/application_flickr_viewer/MainActivity.java
+++ app/src/main/java/com/example/joe/application_flickr_viewer/MainActivity.java
@@ -16,6 +16,8 @@ import android.widget.Toast;
 public class MainActivity extends Activity {
 
     private Button btnOne;
+    private Button btnTwo;
+    private Button btnThree;
 
 
 
@@ -29,12 +31,37 @@ public class MainActivity extends Activity {
             btnOne.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Toast.makeText(MainActivity.this, "Hello",Toast.LENGTH_SHORT).show();
+                Toast.makeText(MainActivity.this, "Developer Information",Toast.LENGTH_SHORT).show();
 
                 Intent i = new Intent(MainActivity.this, activity_search.class);
                 startActivity(i);
             }
         });
+
+        btnTwo = (Button)findViewById(R.id.btnTwo);
+
+        btnTwo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(MainActivity.this, "Search Locations",Toast.LENGTH_SHORT).show();
+
+                Intent i = new Intent(MainActivity.this, Top_Locations.class);
+                startActivity(i);
+            }
+        });
+
+        btnThree = (Button)findViewById(R.id.btnThree);
+
+        btnThree.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(MainActivity.this, "Top Photos",Toast.LENGTH_SHORT).show();
+
+                Intent i = new Intent(MainActivity.this, Top_Photographs.class);
+                startActivity(i);
+            }
+        });
+
     }
 
 
--- app/src/main/java/com/example/joe/application_flickr_viewer/Top_Locations.java
+++ app/src/main/java/com/example/joe/application_flickr_viewer/Top_Locations.java
@@ -8,14 +8,15 @@ import android.widget.ArrayAdapter;
 import android.widget.ImageView;
 import android.widget.ListView;
 import android.widget.TextView;
-
 import java.util.ArrayList;
 import java.util.List;
 
-
 public class Top_Locations extends Activity {
 
-    private ListView listLocations;
+private ListView listLocations;
+private List<String> cities;
+private List<String> countries;
+private List<Integer> flags;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,15 +25,47 @@ public class Top_Locations extends Activity {
 
         listLocations = (ListView) findViewById(R.id.listLocations);
 
+        cities = new ArrayList<>();
+        cities.add("London");
+        cities.add("Paris");
+        cities.add("Berlin");
+        cities.add("Madrid");
+        cities.add("Rome");
+        cities.add("Dublin");
+        cities.add("Brussels");
+        cities.add("Copenhagen");
+        cities.add("Athens");
+        cities.add("Amsterdam");
+
+        countries = new ArrayList<>();
+        countries.add("England");
+        countries.add("France");
+        countries.add("Germany");
+        countries.add("Spain");
+        countries.add("Italy");
+        countries.add("Ireland");
+        countries.add("Belgium");
+        countries.add("Denmark");
+        countries.add("Greece");
+        countries.add("Netherlands");
+
+        flags = new ArrayList<>();
+        flags.add(R.drawable.england);
+        flags.add(R.drawable.france);
+        flags.add(R.drawable.germany);
+        flags.add(R.drawable.spain);
+        flags.add(R.drawable.italy);
+        flags.add(R.drawable.ireland);
+        flags.add(R.drawable.belgium);
+        flags.add(R.drawable.denmark);
+        flags.add(R.drawable.greece);
+        flags.add(R.drawable.netherlands);
+
+        LocationAdapter adapter = new LocationAdapter(cities);
+
+        listLocations.setAdapter(adapter);
+
     }
-
-    private List<String> cities;
-    private List<String> countries;
-    private List<Integer> flags;
-
-    LocationAdapter adapter = new LocationAdapter(cities);
-
-    listLocations.setAdapter(adapter);
 
     private class LocationAdapter extends ArrayAdapter<String> {
 
@@ -48,17 +81,19 @@ public class Top_Locations extends Activity {
                         R.layout.location_list, null);
             }
 
-            cities = new ArrayList<String>();
-            cities.add("Hong Kong");
-
             ImageView imgFlag = (ImageView)convertView.findViewById(R.id.imgFlag);
             TextView lblCity = (TextView)convertView.findViewById(R.id.lblCity);
             TextView lblCountry = (TextView)convertView.findViewById(R.id.lblCountry);
 
             String city = cities.get(position);
+            String country = countries.get(position);
+            int flag = flags.get(position);
 
-
+            lblCity.setText(city);
+            lblCountry.setText(country);
+            imgFlag.setImageResource(flag);
             return convertView;
+
         }// end get view
 
     }// end adapter class
